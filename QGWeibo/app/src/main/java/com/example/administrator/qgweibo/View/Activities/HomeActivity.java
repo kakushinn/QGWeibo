2d80171d61bbed11a44f24895b0d212322df6529
kakushinn <guoc_hen@hotmail.com>
2017-02-27 00:17:32 +0800

    导航栏scrollTo部分测试代码

--- QGWeibo/app/src/main/java/com/example/administrator/qgweibo/View/Activities/HomeActivity.java
+++ QGWeibo/app/src/main/java/com/example/administrator/qgweibo/View/Activities/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.administrator.qgweibo.View.Activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.HorizontalScrollView;
 import android.widget.RadioGroup;
 
 import com.example.administrator.qgweibo.R;
@@ -13,12 +15,24 @@ import com.example.administrator.qgweibo.R;
 public class HomeActivity extends BaseActivity {
 
     public Toolbar homeToolbar;
+    public HorizontalScrollView scrollHorizontal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         homeToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(homeToolbar);
-        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        scrollHorizontal = (HorizontalScrollView)findViewById(R.id.scrollHorizontal);
+        int width = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);
+        int height = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);
+        scrollHorizontal.measure(width,height);
+        scrollHorizontal.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollHorizontal.   scrollTo(scrollHorizontal.getMeasuredWidth(),scrollHorizontal.getMeasuredHeight());
+            }
+        });
+        int a = scrollHorizontal.getMeasuredWidth();
+        int b = scrollHorizontal.getMeasuredHeight();
     }
 }
